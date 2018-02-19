38b51a903d0d659816e4730c24f6861bf097541a
pihome-shc <info@pihome.eu>
2018-02-19 00:43:30 +0000

    MySQL Database setting Variables, other small improvements
    MySQL Database setting Variables, other small improvements.

--- cron/gpio_ds18b20.py
+++ cron/gpio_ds18b20.py
@@ -29,23 +29,27 @@ print " " + bc.ENDC
 logging.basicConfig(filename='/var/www/cron/logs/DS18B20_error.log', level=logging.DEBUG, format='%(asctime)s %(levelname)s %(name)s %(message)s')
 logger=logging.getLogger(__name__)
 
-# Add in the w1_gpio and w1_therm modules
+#Add in the w1_gpio and w1_therm modules
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
-print bc.dtm + time.ctime() + bc.ENDC + ' - DS Temperature Sensors Script Started'
+#Database Settings Variables 
+dbhost = 'localhost'
+dbuser = 'root'
+dbpass = 'passw0rd'
+dbname = 'pihome'
+
+print bc.dtm + time.ctime() + bc.ENDC + ' - DS18B20 Temperature Sensors Script Started'
 print "-" * 68
 
-#function for storing readings into MySql
+#Function for Storing DS18B20 Temperature Readings into MySQL
 def insertDB(IDs, temperature):
 	try:
-		con = mdb.connect('localhost', 'root', 'passw0rd', 'pihome');
+		con = mdb.connect(dbhost, dbuser, dbpass, dbname);
 		cursor = con.cursor()
 		for i in range(0,len(temperature)):
 			print bc.dtm + time.ctime() + bc.ENDC + ' - Sensors ID' + bc.grn, IDs[i], bc.ENDC + 'Temperature' + bc.grn, temperature[i], bc.ENDC
-			sql = "INSERT INTO temp_reading(sensor_id, date, time, value) \
-			VALUES ('%s', '%s', '%s', '%s' )" % \
-			(IDs[i], time.strftime("%Y-%m-%d"), time.strftime("%H:%M"), temperature[i])
+			sql = "INSERT INTO messages_in(node_id, child_id, sub_type, payload, datetime) VALUES ('%s', '%s', '%s', '%s', '%s')" % (IDs[i], '0', '0', temperature[i], time.strftime("%Y-%m-%d %H:%M"))
 			cursor.execute(sql)
 			sql = []
 			con.commit()
@@ -53,7 +57,7 @@ def insertDB(IDs, temperature):
 	except mdb.Error, e:
 		logger.error(e)
 
-#get readings from sensors and store them to MySql
+#Read DS18B20 Sensors and Save Them to MySQL
 while True:
 	temperature = []
 	IDs = []
