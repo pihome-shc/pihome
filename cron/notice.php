c7d2cbd475691437353a7a31f084fa1d0ea8ad6a
pihome-shc <info@pihome.eu>
2019-06-15 18:37:17 +0100

    Update notice.php
    #27 this can be improved just initial work,

--- cron/notice.php
+++ cron/notice.php
@@ -11,15 +11,16 @@ echo "  |_|      |_| |_|  |_|  \___/  |_| |_| |_|  \___| \n";
 echo " \033[0m \n";
 echo "     \033[45m S M A R T   H E A T I N G   C O N T R O L \033[0m \n";
 echo "\033[31m";
-echo "*******************************************************\n";
+echo "********************************************************\n";
 echo "*   Notice Script Version 0.01 Build Date 14/09/20189  *\n";
-echo "*   Update on 14/09/2019                               *\n";
+echo "*   Update on 15/06/2019                               *\n";
 echo "*                                Have Fun - PiHome.eu *\n";
 echo "*******************************************************\n";
 echo " \033[0m \n";
 
 require_once(__DIR__.'../../st_inc/connection.php');
 require_once(__DIR__.'../../st_inc/functions.php');
+require_once(__DIR__.'../../st_inc/class.phpmailer.php');
 $line = "------------------------------------------------------------------\n";
 
 //Set php script execution time in seconds
@@ -30,19 +31,23 @@ echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - Notice Script Started \n";
 echo $line;
 
 echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - Checking Gateway Communication \n"; 
-$query = "select * FROM gateway_logs WHERE pid_datetime >= NOW() - INTERVAL 5 MINUTE;";
-$resulta = $conn->query($query);
-if (mysqli_num_rows($resulta) > 4){
-	$gw_restarted = mysqli_num_rows($resulta);
-	echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - Gateway Connection Lost in Last 5 minutes: ".$gw_restarted." \n"; 
-	$query = "INSERT INTO notice (sync, `purge`, datetime, message, status) VALUES ('0', '0', '{$date_time}', 'Gateway Connection Lost in Last 5 Minutes ".$gw_restarted."', '1');";
-	$result = $conn->query($query);
+//Check Gateway Logs for last 10 minuts and start search for gateway connected failed. 
+$query = "select count(*) as cnt from  gateway_logs where pid_datetime >= DATE_SUB(NOW(),INTERVAL 10 MINUTE);";
+$result = $conn->query($query);
+$gl_row = mysqli_fetch_array($result);
+$gl_cnt = $gl_row['cnt'];
+if ($gl_cnt > 10){
+	echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - Gateway Connection Lost in Last 10 minutes: ".$gl_cnt." \n"; 
+	$query = "INSERT INTO notice (sync, `purge`, datetime, message, status) VALUES ('0', '0', '{$date_time}', 'Gateway Connection Lost in Last 10 Minutes ".$gl_cnt."', '1');";
+	$conn->query($query);
 }
-echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - Gateway Check Finished \n"; 
+echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - Gateway Notice Finished \n"; 
 echo $line;
 
+//*************************************************************************************************************
+//Temperature Sensors Last Seen status 
 echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - Checking Temperature Sensors Communication \n"; 
-$query = "select * FROM nodes WHERE name = 'Temperature Sensor' AND last_seen <= NOW() - INTERVAL 5 MINUTE;";
+$query = "select * FROM nodes WHERE name = 'Temperature Sensor' AND last_seen <= DATE_SUB(NOW(),INTERVAL 10 MINUTE);";
 $resultb = $conn->query($query);
 if (mysqli_num_rows($resultb) != 0){
 	while ($row = mysqli_fetch_assoc($resultb)) {
@@ -58,6 +63,8 @@ if (mysqli_num_rows($resultb) != 0){
 echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - Temperature Sensor Check Finished \n"; 
 echo $line;
 
+//*************************************************************************************************************
+//Boiler Controller Last Seen. 
 echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - Checking Boiler Controller Communication \n"; 
 $query = "select * FROM nodes WHERE name = 'Boiler Relay' AND last_seen <= NOW() - INTERVAL 5 MINUTE;";
 $resultc = $conn->query($query);
@@ -75,6 +82,8 @@ if (mysqli_num_rows($resultc) != 0){
 echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - Boiler Controller Relay Check Finished \n"; 
 echo $line;
 
+//*************************************************************************************************************
+//Zone Controller Last Seen.
 echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - Checking Zone Controller Communication \n"; 
 $query = "select * FROM nodes WHERE name = 'Zone Controller Relay' AND node_id != '0' AND last_seen <= NOW() - INTERVAL 5 MINUTE;";
 $resultd = $conn->query($query);
@@ -91,19 +100,15 @@ if (mysqli_num_rows($resultd) != 0){
 }
 echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - Zone Controller Relay Check Finished \n"; 
 echo $line;
-
+//*************************************************************************************************************
+//Check CPU Temperature from last one hour if it was over 50c
 echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - Checking CPU Temperature \n"; 
-$query = "select * FROM messages_in_view_24h WHERE node_id = '0' order by `datetime` desc Limit 1;";
+$query = "select * FROM messages_in_view_24h WHERE node_id = '0' AND payload > 50 AND DATETIME >= DATE_SUB(NOW(),INTERVAL 60 MINUTE);";
 $resulta = $conn->query($query);
 if (mysqli_num_rows($resulta) != 0){
-	while ($row = mysqli_fetch_assoc($resultd)) {
-		$datetime=$row['datetime'];
-		$payload=$row['payload'];
-		
-		echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - CPU Temperature is very high: ".$payload." \n"; 
-		$query = "INSERT INTO notice (sync, `purge`, datetime, message, status) VALUES ('0', '0', '{$date_time}', 'CPU Temperature is very high ".$payload."', '1');";
-		$result = $conn->query($query);
-	}
+	echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - CPU Temperature is very high: \n"; 
+	$query = "INSERT INTO notice (sync, `purge`, datetime, message, status) VALUES ('0', '0', '{$date_time}', 'Over 50c CPU Temperature Recorded in last one Hour', '1');";
+	$conn->query($query);
 }
 echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - CPU Temperature Check Finished \n"; 
 echo $line;
