72a2d8f66e28506a1ff66f031b0961643c78fbe4
pihome-shc <33334232+pihome-shc@users.noreply.github.com>
2019-06-18 00:02:56 +0100

    Update PiHome_Boiler_Controller.ino
    This will add delay of 1 minute to boiler start time so pressure in systems have somewhere to go if zone valve slow to response.

--- Arduino Sketches/PiHome_Boiler_Controller/PiHome_Boiler_Controller.ino
+++ Arduino Sketches/PiHome_Boiler_Controller/PiHome_Boiler_Controller.ino
@@ -9,7 +9,7 @@
 // *****************************************************************
 // *              Boiler Controller Relay Sketch                   *
 // *            Version 0.31 Build Date 09/01/2019                 *
-// *            Last Modification Date 09/05/2019                  *
+// *            Last Modification Date 17/06/2019                  *
 // *                                          Have Fun - PiHome.eu *
 // *****************************************************************
 
@@ -22,7 +22,7 @@
 //#define MY_RADIO_RFM69
 //#define MY_RADIO_RFM95
 
-//IRQ Pin on Arduino
+//Define this to use the IRQ pin of the RF24 module (optional). 
 //#define MY_RF24_IRQ_PIN 8
 
 // Set LOW transmit power level as default, if you have an amplified NRF-module and
@@ -40,7 +40,13 @@
 #define MY_NODE_ID 100
 
 //RF24_250KBPS for 250kbs, RF24_1MBPS for 1Mbps, or RF24_2MBPS for 2Mbps
-#define RF24_DATARATE 	   RF24_250KBPS
+#define MY_RF24_DATARATE RF24_250KBPS
+
+//Enable Signing <Make Sure you Change Password>
+//#define MY_SIGNING_SIMPLE_PASSWD "pihome"
+
+//Enable Encryption This uses less memory, and hides the actual data. <Make Sure you Change Password>
+//#define MY_ENCRYPTION_SIMPLE_PASSWD "pihome"
 
 // Enable repeater functionality for this node
 //#define MY_REPEATER_FEATURE
@@ -65,6 +71,8 @@
 #define NUMBER_OF_RELAYS 1 // Total number of attached relays
 #define RELAY_ON 0  // GPIO value to write to turn on attached relay
 #define RELAY_OFF 1 // GPIO value to write to turn off attached relay
+
+int oldStatus = RELAY_OFF;
 
 void before()
 {
@@ -91,7 +99,6 @@ void presentation()
 	}
 }
 
-
 void loop(){
 
 }
@@ -100,18 +107,43 @@ void receive(const MyMessage &message)
 {
 	// We only expect one type of message from controller. But we better check anyway.
 	if (message.type==V_STATUS) {
-	
-		// Change relay state
-		digitalWrite(message.sensor-1+RELAY_1, message.getBool()?RELAY_ON:RELAY_OFF);
-	
-		// Store state in eeprom - we dont need to save relay state as controller take care of this, 
-		// saveState(message.sensor, message.getBool());
+		
+		//digitalWrite(message.sensor-1+RELAY_1, message.getBool()?RELAY_ON:RELAY_OFF);
+		int RELAY_status = (message.sensor-1+RELAY_1, message.getBool()?RELAY_ON:RELAY_OFF);
+		
 		// Write some debug info
 		#ifdef MY_DEBUG
-			Serial.print("Incoming change for sensor:");
-			Serial.print(message.sensor);
-			Serial.print(", New status: ");
-			Serial.println(message.getBool());
+			Serial.print("Relay Status Received: ");
+			Serial.println(RELAY_status);
+			Serial.print("Relay Old Status: ");
+			Serial.println(oldStatus);
+			Serial.print(" \n");
 		#endif
+		if (oldStatus == RELAY_ON && RELAY_status == RELAY_ON){
+			//Change relay state to On 
+			digitalWrite(message.sensor-1+RELAY_1, RELAY_ON);
+			oldStatus = RELAY_status;
+		}else {
+			// Change relay state to Off
+			digitalWrite(message.sensor-1+RELAY_1, RELAY_OFF);
+			oldStatus = RELAY_status;
+		}
+		
+		/*
+		message.sensor-1+RELAY_1
+		message received for child id 1 but we minus 1 to make it 0 and then add relay_1 variable value for pin number so we have correct pin number
+		
+		message.getBool()
+		check if message type 
+		
+		?
+		Conditional ternary operator
+		
+		RELAY_ON:RELAY_OFF
+		after Conditional ternary operator it check if value is 0 or 1 and condition get translated to relay on or relay off 
+		*/
+
+		// Store state in eeprom - we dont need to save relay state as controller take care of this, 
+		// saveState(message.sensor, message.getBool());
 	}
 }
