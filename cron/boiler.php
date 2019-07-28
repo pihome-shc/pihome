d156dfbe3567b5ee0227b47854646e4be5ab2a1e
twa127 <46624596+twa127@users.noreply.github.com>
2019-07-28 12:22:36 +0100

    Add files via upload

--- cron/boiler.php
+++ cron/boiler.php
@@ -41,7 +41,7 @@ $boiler_node_id = $row['node_id'];
 $boiler_node_child_id = $row['node_child_id'];
 $boiler_hysteresis_time = $row['hysteresis_time'];
 $boiler_max_operation_time = $row['max_operation_time'];
-$boiler_goip_pin = $row['gpio_pin'];
+$boiler_gpio_pin = $row['gpio_pin'];
 
 //query to check away status
 $query = "SELECT * FROM away LIMIT 1";
@@ -344,12 +344,12 @@ if (in_array("1", $boiler)) {
 	$query = "UPDATE boiler SET sync = '0', fired_status = '{$new_boiler_status}' WHERE id ='1' LIMIT 1";
 	$conn->query($query);
 
-	if (!empty($boiler_goip_pin)){
+	if (!empty($boiler_gpio_pin)){
 		/***************************************************************************************
 		Boiler Wired to Raspberry Pi GPIO Section: Make sure you have WiringPi installed.
 		****************************************************************************************/
-		exec("/usr/local/bin/gpio write ".$boiler_goip_pin ." ".$relay_on );
-		exec("/usr/local/bin/gpio mode ".$boiler_goip_pin ." out");
+		exec("/usr/local/bin/gpio write ".$boiler_gpio_pin ." ".$relay_on );
+		exec("/usr/local/bin/gpio mode ".$boiler_gpio_pin ." out");
 	} else {
 	        /***************************************************************************************
         	GAS Boiler Wirelss Section:     MySensors Wireless Relay module for your GAS Boiler control
@@ -360,7 +360,7 @@ if (in_array("1", $boiler)) {
 	}
 
 	echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - Boiler Node ID: \033[41m".$boiler_node_id."\033[0m Child ID: \033[41m".$boiler_node_child_id."\033[0m \n";
-	echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - Boiler GIOP: \033[41m".$boiler_goip_pin. "\033[0m Status: \033[41m".$relay_on."\033[0m (0=On, 1=Off) \n";
+	echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - Boiler GIOP: \033[41m".$boiler_gpio_pin. "\033[0m Status: \033[41m".$relay_on."\033[0m (0=On, 1=Off) \n";
 
 	if ($boiler_fire_status != $new_boiler_status){
 		//insert date and time into boiler log table so we can record boiler start date and time.
@@ -388,12 +388,12 @@ if (in_array("1", $boiler)) {
 	$query = "UPDATE boiler SET sync = '0', fired_status = '{$new_boiler_status}' WHERE id ='1' LIMIT 1";
 	$conn->query($query);
 
-	if (!empty($boiler_goip_pin)){
+	if (!empty($boiler_gpio_pin)){
 		/***************************************************************************************
 		Boiler Wired to Raspberry Pi GPIO Section: Make sure you have WiringPi installed.
 		****************************************************************************************/
-		exec("/usr/local/bin/gpio write ".$boiler_goip_pin ." ".$relay_off );
-		exec("/usr/local/bin/gpio mode ".$boiler_goip_pin ." out");
+		exec("/usr/local/bin/gpio write ".$boiler_gpio_pin ." ".$relay_off );
+		exec("/usr/local/bin/gpio mode ".$boiler_gpio_pin ." out");
 	} else {
 	        /***************************************************************************************
         	GAS Boiler Wirelss Section:     MySensors Wireless Relay module for your GAS Boiler control
@@ -404,7 +404,7 @@ if (in_array("1", $boiler)) {
 	}
 
 	echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - Boiler Node ID: \033[41m".$boiler_node_id."\033[0m Child ID: \033[41m".$boiler_node_child_id."\033[0m \n";
-	echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - Boiler GIOP: \033[41m".$boiler_goip_pin. "\033[0m Status: \033[41m".$relay_off."\033[0m (0=On, 1=Off) \n";
+	echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - Boiler GIOP: \033[41m".$boiler_gpio_pin. "\033[0m Status: \033[41m".$relay_off."\033[0m (0=On, 1=Off) \n";
 	if ($boiler_fire_status != $new_boiler_status){
 		//Update last record with boiler stop date and time in boiler log table.
 		$query = "UPDATE boiler_logs SET stop_datetime = '{$date_time}', stop_cause = '{$stop_cause}' ORDER BY id DESC LIMIT 1";
