ce2aec93e09f5dd0022f97e5636005a21442f478
twa127 <46624596+twa127@users.noreply.github.com>
2019-08-29 16:19:52 +0100

    Update settingslist.php
    Enable selection of heating zone graphs

--- settingslist.php
+++ settingslist.php
@@ -79,7 +79,13 @@ $("#ajaxModal").on("show.bs.modal", function(e) {
 							<h3 class="buttontop"><small><?php echo $lang['language']; ?></small></h3>
 							<h3 class="degre" ><i class="fa fa-language fa-1x blueinfo"></i></h3>
 							<h3 class="status"></small></h3>
-                            </button>
+                             </button>
+
+                                                        <button class="btn btn-default btn-circle btn-xxl mainbtn animated fadeIn" data-href="#" data-toggle="modal" data-target="#zone_graph">
+                                                        <h3 class="buttontop"><small><?php echo $lang['graph']; ?></small></h3>
+                                                        <h3 class="degre" ><i class="fa fa-bar-chart fa-1x blueinfo"></i></h3>
+                                                        <h3 class="status"></small></h3>
+                           </button>
 
 							<button class="btn btn-default btn-circle btn-xxl mainbtn animated fadeIn" data-href="edit_boiler.php" data-toggle="modal" data-target="#boiler">
 							<h3 class="buttontop"><small><?php echo $lang['boiler']; ?></small></h3>
