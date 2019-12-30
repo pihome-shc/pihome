4fb71e7950eebd75422cf021b922d9b268b1694d
sandreialexandru <45868553+sandreialexandru@users.noreply.github.com>
2019-12-30 12:57:30 +0200

    Update MySensors_WiFi_Gateway_ESP8266.ino
    ESP8266 WiFi Connection manager with fallback web configuration portal
    
    Configure the network credentials in a esp8266 without requiring to modify and download the firmware again
    
    More info https://github.com/tzapu/WiFiManager

--- Arduino Sketches/MySensors_WiFi_Gateway_ESP8266/MySensors_WiFi_Gateway_ESP8266.ino
+++ Arduino Sketches/MySensors_WiFi_Gateway_ESP8266/MySensors_WiFi_Gateway_ESP8266.ino
@@ -101,16 +101,42 @@
 
 
 #define MY_GATEWAY_ESP8266
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
 
-#define MY_ESP8266_SSID "my_wifi_id"
-#define MY_ESP8266_PASSWORD "my_wifi_password"
+//needed for library
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
+
+
+void setup() {
+    // put your setup code here, to run once:
+    Serial.begin(115200);
+
+    //WiFiManager
+    //Local intialization. Once its business is done, there is no need to keep it around
+    WiFiManager wifiManager;
+    //reset saved settings
+    //wifiManager.resetSettings();
+    
+    //set custom ip for portal
+    //wifiManager.setAPStaticIPConfig(IPAddress(10,0,1,1), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
+
+    //fetches ssid and pass from eeprom and tries to connect
+    //if it does not connect it starts an access point with the specified name
+    //here  "AutoConnectAP"
+    //and goes into a blocking loop awaiting configuration
+    wifiManager.autoConnect("AutoConnectAP");
+    //or use this for auto generated name ESP + ChipID
+    //wifiManager.autoConnect();
+
+    
+    //if you get here you have connected to the WiFi
+    Serial.println("connected...yeey :)");
+}
 
 // Enable UDP communication
 //#define MY_USE_UDP
-
-// Set the hostname for the WiFi Client. This is the hostname
-// it will pass to the DHCP server if not static.
-#define MY_ESP8266_HOSTNAME "PiHome_Gateway"
 
 // Enable MY_IP_ADDRESS here if you want a static ip address (no DHCP)
 //#define MY_IP_ADDRESS 192,168,99,4
@@ -153,13 +179,8 @@
 #include <WiFiUdp.h>
 #endif
 
-#include <ESP8266WiFi.h>
-
 #include <MySensors.h>
 
-void setup()
-{
-}
 
 void presentation()
 {
