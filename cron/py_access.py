3d838595d41e185368758d640026ac361032652e
sandreialexandru <45868553+sandreialexandru@users.noreply.github.com>
2020-01-14 20:21:29 +0200

    Update py_access.py

--- cron/py_access.py
+++ cron/py_access.py
@@ -1,5 +1,11 @@
-#Database Settings Variables 
-host = 'localhost'
-user = 'root'
-password = 'passw0rd'
-dbname = 'pihome'
+#Edit line 7 as needed
+_connection = None
+
+def get_connection():
+    global _connection
+    if not _connection:
+        _connection = MySQLdb.connect(host="localhost", user = "root", password = "passw0rd", dbname = "pihome")
+    return _connection
+
+# List of stuff accessible to importers of this module. Just in case
+__all__ = [ 'getConnection' ]
