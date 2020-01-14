9d3511b71c21296784db8fcb34a7ee5a2fb474dc
sandreialexandru <45868553+sandreialexandru@users.noreply.github.com>
2020-01-14 19:09:30 +0200

    Update setup.php

--- setup.php
+++ setup.php
@@ -36,20 +36,7 @@ if ($version[0] > 7){
 	echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - PHP Version \033[41m".phpversion()."\033[0m Looks OK \n";
 }
 
-//*********************************************************
-//* Modify Following variable according to your settings  *
-//*********************************************************
-$hostname = 'localhost';
-$dbname   = 'pihome';
-$dbusername = 'pihomedbadmin';
-$dbpassword = 'pihome2018';
-$connect_error = 'Sorry We are Experiencing MySQL Database Connection Problem...';
-
-//Test Connection to MySQL Server with Given Username & Password 
-$conn = new mysqli($hostname, $dbusername, $dbpassword);
-if ($conn->connect_error){
-	die('Database Connecction Failed with Error: '.$conn->connect_error);
-}
+require_once(__DIR__.'st_inc/connection.php');
 
 $db_selected = mysqli_select_db($conn, $dbname);
 if (!$db_selected) {
