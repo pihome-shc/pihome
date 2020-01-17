4bc71c3ddfbec2aaf9c0aa6c69a629f4fbdaeccb
sandreialexandru <45868553+sandreialexandru@users.noreply.github.com>
2020-01-17 17:19:13 +0200

    Update schedule_edit.php

--- schedule_edit.php
+++ schedule_edit.php
@@ -208,9 +208,8 @@ if($row['tz_status'] == 1){
     <div class="help-block with-errors"></div></div>
     
 	<div class="slidecontainer">
-		<h4><?php echo $lang['temperature']; ?>: <span id="val<?php echo $row["id"];?>" style="display: inline-flex !important; font-size:18px !important;"><output name="show_temp_val" id="temp<?php echo $row["id"];?>" style="padding-top:0px !important; font-size:18px !important;">15.0</output>&deg;</span></h4><br>
-		<input type="range" min="<?php echo $min; ?>" max="<?php echo $max; ?>" step="0.5" value="15.0" class="slider" id="bb<?php echo $row["id"];?>" name="temp[<?php echo $row["id"];?>]" oninput="document.getElementById('temp<?php echo $row["id"];?>').innerText = parseFloat(this.value);temp<?php echo $row["id"];?>=parseFloat(this.value)">
-	
+		<h4><?php echo $lang['temperature']; ?>: <span id="val<?php echo $row["zone_id"];?>" style="display: inline-flex !important; font-size:18px !important;"><output name="show_temp_val" id="temp<?php echo $row["zone_id"];?>" style="padding-top:0px !important; font-size:18px !important;">15.0</output>&deg;</span></h4><br>
+		<input type="range" min="<?php echo $min; ?>" max="<?php echo $max; ?>" step="0.5" value="15.0" class="slider" id="bb<?php echo $row["zone_id"];?>" name="temp[<?php echo $row["zone_id"];?>]" oninput="document.getElementById('temp<?php echo $row["zone_id"];?>').innerText = parseFloat(this.value);temp<?php echo $row["zone_id"];?>=parseFloat(this.value)">
 	</div>
     </div></div>
 
