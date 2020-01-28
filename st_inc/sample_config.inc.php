9a4b2ea7de08278ad2db593eafb7c088206c036d
harvybob <rajuvora@hotmail.com>
2020-01-28 08:58:14 +0000

    Update sample_config.inc.php

--- st_inc/sample_config.inc.php
+++ st_inc/sample_config.inc.php
@@ -1,4 +1,4 @@
-/* rename this file to _config.inc.php and correct the connection details below */
+/* rename this file to _config.inc.php and correct the connection details below  and remove this line*/
 <?php
 $hostname = 'localhost';
 $dbname   = 'pihome';
