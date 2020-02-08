72dc3f0289f23eccb24d2ff24c8f1ca425852e8d
twa127 <46624596+twa127@users.noreply.github.com>
2020-02-08 14:28:26 +0000

    Update MySQL_View.sql

--- MySQL_Database/MySQL_View.sql
+++ MySQL_Database/MySQL_View.sql
@@ -41,7 +41,7 @@ Drop View if exists zone_view;
 CREATE VIEW zone_view AS
 select zone.status, zone.sync, zone.id, zone.index_id, zone.name, zone.type, zone.graph_it, zone.max_c, zone.max_operation_time, zone.hysteresis_time,
 zone.sp_deadband, sid.node_id as sensors_id, zone.sensor_child_id,
-ctype.`type` AS controller_type, cid.node_id as controler_id, zone.controler_child_id,
+ctype.`type` AS controller_type, cid.node_id as controler_id, cid.child_id_1 AS controler_child_id,
 lasts.last_seen, msv.ms_version, skv.sketch_version
 from zone
 join nodes sid on zone.sensor_id = sid.id
@@ -55,7 +55,7 @@ where zone.`purge` = '0';
 -- Boiler View
 Drop View if exists boiler_view;
 CREATE VIEW boiler_view AS
-select boiler.status, boiler.sync, boiler.`purge`, boiler.fired_status, boiler.name, ctype.`type` AS controller_type, nodes.node_id, boiler.node_child_id, boiler.hysteresis_time, boiler.max_operation_time
+select boiler.status, boiler.sync, boiler.`purge`, boiler.fired_status, boiler.name, ctype.`type` AS controller_type, nodes.node_id, nodes.child_id_1 AS node_child_id, boiler.hysteresis_time, boiler.max_operation_time
 from boiler
 join nodes on boiler.node_id = nodes.id
 join nodes ctype on boiler.node_id = ctype.id
