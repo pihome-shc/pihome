43fd7459196eae5b548d942579277357bb57190b
twa127 <46624596+twa127@users.noreply.github.com>
2020-02-24 21:18:23 +0000

    Update languages.php

--- languages.php
+++ languages.php
@@ -1,9 +1,12 @@
 <?php 
 require_once(__DIR__.'/st_inc/session.php'); 
+require_once(__DIR__.'/st_inc/connection.php');
 
 //Set PiHome Language when user click on Lanage url
 if(isset($_GET['lang'])) {
 	$lang = $_GET['lang'];
+	$query = "UPDATE `system` SET `language`='" . $_GET['lang'] . "';";
+        $conn->query($query);
 	setcookie("PiHomeLanguage", $lang, time()+(3600*24*90));
 	header("Location: " . $_SERVER['HTTP_REFERER']);
 	exit;
