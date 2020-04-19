d9dd17ee1f1b4e23ebd6898680785881d2756cb1
twa127 <46624596+twa127@users.noreply.github.com>
2020-04-19 13:01:48 +0100

    Add files via upload

---
+++ add_on/homekit/config_json.py
@@ -0,0 +1,79 @@
+import json
+import MySQLdb as mdb
+import ConfigParser
+import time
+import collections
+
+# Initialise the database access variables
+config = ConfigParser.ConfigParser()
+config.read('/var/www/st_inc/db_config.ini')
+dbhost = config.get('db', 'hostname')
+dbuser = config.get('db', 'dbusername')
+dbpass = config.get('db', 'dbpassword')
+dbname = config.get('db', 'dbname')
+
+# Read existing config.json in to a python dictionary
+src = "/var/lib/homebridge/config.json"
+with open(src, "r") as read_file:
+        config = json.load(read_file)
+
+# get zone names from the database
+con = mdb.connect(dbhost, dbuser, dbpass, dbname)
+cur = con.cursor()
+cur.execute("SELECT * FROM zone_view WHERE status  = 1 or graph_it = 1")
+result = cur.fetchall()
+cur.close()
+con.close()
+
+# Fill list using template for each active zone
+zonelist =[]
+d = collections.OrderedDict()
+d['platform'] = 'config'
+d['name'] = 'Config'
+d['port'] = '8581'
+zonelist.append(d)
+
+d = collections.OrderedDict()
+d['platform'] = 'HttpWebHooks'
+d['webhook_port'] = '51828'
+d['cache_directory'] = './.node-persist/storage'
+d['https'] = False
+
+# Add switches for active zone controllers
+switches = []
+for row in result:
+        if row[0] == 1:
+                sub_d = collections.OrderedDict()
+                sub_d['id'] = 'switch' + str(row[2])
+                sub_d['name'] = row[4] + ' Zone'
+                sub_d['on_url'] = 'http://127.0.0.1/api/boostSet?zonename=' + row[4] + '&state=1'
+                sub_d['on_method'] = 'GET'
+                sub_d['off_url'] = 'http://127.0.0.1/api/boostSet?zonename=' + row[4] + '&state=0'
+                sub_d['off_method'] = 'GET'
+                switches.append(sub_d)
+d['switches'] = switches
+
+# Add sensors for zones with graph_it set
+sensors = []
+for row in result:
+        sub_d = collections.OrderedDict()
+        sub_d['id'] = 'sensor' + str(row[2])
+        sub_d['name'] = row[4] + ' Temperature'
+        sub_d['type'] = 'temperature'
+        sensors.append(sub_d)
+d['sensors'] = sensors
+zonelist.append(d)
+
+# Add list to dictionary
+config["platforms"] = zonelist
+
+# If the 'accessories' key exist in dictionary then delete it using del.
+if "accessories" in config:
+        del config['accessories']
+# Add empty accessories block
+config["accessories"] = []
+
+# Write updated config.json file
+with open(src, "w") as write_file:
+        json.dump(config, write_file, indent = 4)
+
---
+++ add_on/homekit/install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+echo "Enabling mod_rewrite"
+sudo a2enmod rewrite
+echo "Backing Up and Modifying /etc/apache2/sites-available/000-default.conf"
+FILE=/etc/apache2/sites-available/000-default.conf
+if grep -q "<Directory /var/www/api>" $FILE
+then
+    echo "000-default.conf Already Modified"
+else
+    sudo cp -a -- "$FILE" "$FILE-$(date +"%Y%m%d-%H%M%S")"
+    sudo awk '/DocumentRoot/{print $0 RS "" RS "        <Directory /var/www/api>"\
+      RS "              Options Indexes FollowSymLinks" \
+      RS "              AllowOverride All" \
+      RS "              Require all granted" \
+      RS "        </Directory>";next}1' $FILE > tmp && mv tmp $FILE
+
+    echo "Restarting the apache service"
+    sudo systemctl restart apache2
+fi
+
+echo "Installing the WebHooks Plugin"
+sudo npm install -g homebridge-http-webhooks
+
+echo "Checking For WebHooks Cache Directory"
+DIR="/var/lib/homebridge/.node-persist/storage/"
+if [ -d "$DIR" ]; then
+  echo "Directory Found, Checking if Empty"
+  FILE=""
+  if [ "$(ls -A $DIR)" ]; then
+     echo "Deleting existing Files"
+     sudo rm -f -- /var/lib/homebridge/.node-persist/storage/*
+  else
+    echo "$DIR is Empty"
+  fi
+else
+    echo "Creating Directory, Changing Ownership and Permissions"
+    sudo mkdir $DIR
+    sudo chown homebridge:homebridge $DIR
+    sudo chmod 755 $DIR
+fi
+echo "Backing Up and Updateing /var/lib/homebridge/config.json"
+echo "Adding WebHooks Plugin and  Accessories for Each Zone"
+FILE=/var/lib/homebridge/config.json
+sudo cp -a -- "$FILE" "$FILE-$(date +"%Y%m%d-%H%M%S")"
+/usr/bin/python config_json.py
+echo "Restarting Homebridge"
+sudo hb-service restart
