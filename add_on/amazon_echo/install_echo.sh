432a9fa863a18be23ee6cae9cb83abc13b9ec464
twa127 <46624596+twa127@users.noreply.github.com>
2020-05-15 09:57:56 +0100

    Add files via upload

--- add_on/amazon_echo/install_echo.sh
+++ add_on/amazon_echo/install_echo.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 
-# check if python-requests is installed
-echo "Checking if python-requests is Installed"
-var=$(python -c 'import pkgutil; print(1 if pkgutil.find_loader("requests") else 0)')
-if [ "$var" = "0" ]
+echo "Check if pip3 is Installed"
+var=$(which pip3)
+if [ -z "$var" ]
 then
-        echo "python-requests is Not Installed"
-        echo "Check if python-pip is Installed"
-        var=$(which pip)
-        if [ -z "$var" ]
-        then
-            echo "Installing pip"
-            sudo apt-get install python-pip
-        else
-            echo "pip already installed"
-        fi
+  echo "Installing pip3"
+  sudo apt-get install python3-pip
 else
-        echo "python-requests already installed"
+  echo "pip3 already installed"
 fi
+
+echo "Installing Fauxmo"
+pip3 install fauxmo
+
+echo "Changing Privileges for Fauxmo"
+sudo chmod 755 /var/www/add_on/amazon_echo/fauxmo
+sudo chown fauxmo:fauxmo fauxmo
+
+confdir="/etc/fauxmon"
+### Check for dir, if not found create it using the mkdir ##
+[ ! -d "$confdir" ] && mkdir -p "$confdir"
+echo "Backing Up and Updateing /etc/fauxmo/config.json"
+echo "Adding Accessories for Each Zone"
+FILE=/etc/fauxmo/config.json
+sudo cp -a -- "$FILE" "$FILE-$(date +"%Y%m%d-%H%M%S")"
+/usr/bin/python config_json.py
+
+# add unprivileged fauxmo user
+echo "Adding Unprivileged Fauxmo User"
+sudo useradd -r -s /bin/false fauxmo
 
 # check if Unit File already exists
 echo "Checking For Existing Unit File"
@@ -29,17 +40,21 @@ fi
 echo "Creating Unit File: $FILE"
 sudo cat <<EOT >> /lib/systemd/system/pihome_amazon_echo.service
 [Unit]
-Description=Echo
-After=multi-user.target
+Description=Fauxmo
+After=network-online.target
+Wants=network-online.target
 
 [Service]
 Type=simple
-ExecStart=/usr/bin/python -u /var/www/add_on/amazon_echo/echo_pihome.py
-Restart=on-abort
+WorkingDirectory=/var/www/add_on/amazon_echo
+
+ExecStart=/var/www/add_on/amazon_echo/fauxmo -c /etc/fauxmo/config.json -v
+Restart=on-failure
+RestartSec=10s
+User=fauxmo
 
 [Install]
 WantedBy=multi-user.target
-=============================================
 EOT
 
 echo "Enabling the service"
@@ -47,3 +62,4 @@ sudo systemctl daemon-reload
 sudo systemctl enable pihome_amazon_echo.service
 echo "Starting the service"
 sudo systemctl start pihome_amazon_echo.service
+
