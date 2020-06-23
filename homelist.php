80165a56e8c9f51b3baeb277db3311d6d43027fa
aszumski <a.szumski80@gmail.com>
2020-06-23 11:45:50 +0100

    Update homelist.php

--- homelist.php
+++ homelist.php
@@ -153,11 +153,11 @@ require_once(__DIR__.'/st_inc/functions.php');
                         echo '<small class="statusdegree">' . $rval['target'] .'</small>';
                         //Right icon for what/why
                         echo '<small class="statuszoon"><i class="fa ' . $rval['shactive'] . ' ' . $rval['shcolor'] . ' fa-fw"></i></small>';
-						//Overrun Icon
-						if($overrun == 1) {
-							echo '<small class="statuszoon"><i class="fa ion-ios-play-outline orange fa-fw"></i></small>';
-						}						
-						echo '</h3></button>';      //close out status and button
+			//Overrun Icon
+			if($overrun == 1) {
+				echo '<small class="statuszoon"><i class="fa ion-ios-play-outline orange fa-fw"></i></small>';
+			}						
+			echo '</h3></button>';      //close out status and button
 
 			//Zone Schedule listing model
 			echo '<div class="modal fade" id="'.$zone_type.''.$zone_id.'" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
