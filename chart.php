01f737d3b135c5d06de19db23dcf5d22388d5222
twa127 <46624596+twa127@users.noreply.github.com>
2020-06-23 16:21:04 +0100

    Update chart.php

--- chart.php
+++ chart.php
@@ -47,10 +47,10 @@ require_once(__DIR__.'/st_inc/functions.php');
         				</ul>
 	        			<!-- Tab panes -->
         				<div class="tab-content">
-            				<div class="tab-pane fade in active" id="temperature-pills"><br><?php include("daily_usage.php"); ?></div>
+            				<div class="tab-pane fade in active" id="temperature-pills"><br><?php include("chart_dailyusage.php"); ?></div>
             				<div class="tab-pane fade" id="boiler-pills"><br><?php include("boilerlist.php"); ?></div>
-							<div class="tab-pane fade" id="month-pills"><br><?php include("monthusage.php"); ?></div>
-							<div class="tab-pane fade" id="battery-pills"><br><?php include("battery_usage.php"); ?></div>
+							<div class="tab-pane fade" id="month-pills"><br><?php include("chart_monthusage.php"); ?></div>
+							<div class="tab-pane fade" id="battery-pills"><br><?php include("chart_batteryusage.php"); ?></div>
 	        			</div>
 				</div>
         		        <!-- /.panel-body -->
