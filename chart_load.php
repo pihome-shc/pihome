01aff44b4d7f12aba945dfb17a8760faaa0f2549
twa127 <46624596+twa127@users.noreply.github.com>
2020-06-28 18:45:31 +0100

    Update chart_load.php

--- chart_load.php
+++ chart_load.php
@@ -358,34 +358,6 @@ var bsave_minuts = <?php echo json_encode($bsave_minuts); ?>;
 
 <script type="text/javascript">
 // create battery usage dataset based on all available zones
-var bat_level_dataset = [
-<?php
-    $querya ="select * from nodes where `type` = 'MySensor' AND NAME LIKE '%Sensor' AND `min_value` <> 0;";
-    $resulta = $conn->query($querya);
-    $counter = 0;
-    $count = mysqli_num_rows($resulta) + 1;
-    while ($row = mysqli_fetch_assoc($resulta)) {
-        //grab the node id to be displayed in the plot legend
-		$node_id=$row['node_id'];
-		$query="select * from zone_view where sensors_id = '{$node_id}' limit 1;";
-		$result = $conn->query($query);
-		$zone_row = mysqli_fetch_array($result);
-		$zone_name = $zone_row['name'];
-		$label = $zone_name ." - ID ".$node_id;
-		$query="SELECT bat_voltage, bat_level, `update`  FROM nodes_battery WHERE `update` >= last_day(now()) + interval 1 day - interval 3 MONTH AND bat_level is not NULL and node_id = '{$node_id}' GROUP BY Week(`update`), Day(`update`) ORDER BY `update` ASC;";
-        $result = $conn->query($query);
-        // create array of pairs of x and y values for every zone
-        $bat_level = array();
-		//$bat_voltage = array();
-        while ($rowb = mysqli_fetch_assoc($result)) { 
-            $bat_level[] = array(strtotime($rowb['update']) * 1000, $rowb['bat_level']);
-			//$bat_voltage[] = array(strtotime($rowb['update']) * 1000, $rowb['bat_voltage']);
-        }
-        // create dataset entry using distinct color based on zone index(to have the same color everytime chart is opened)
-        echo "{label: \"".$label."\", data: ".json_encode($bat_level).", color: rainbow(".$count.",".++$counter.") }, \n";
-		//echo "{label: \"".$node_id."\", data: ".json_encode($bat_voltage).", color: rainbow(".$count.",".++$counter.") }, \n";
-    }
-?> ];
 var options_bat = {
 	xaxis: { mode: "time", timezone: "browser", timeformat: "%b %Y"},
     series: { lines: { show: true, lineWidth: 1, fill: false}, curvedLines: { apply: true,  active: true,  monotonicFit: true } },
