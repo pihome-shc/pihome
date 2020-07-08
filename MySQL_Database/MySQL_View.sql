6cbc5c51d60255e0c9f4fe992e0d1acdf47c8dfa
twa127 <46624596+twa127@users.noreply.github.com>
2020-07-08 10:22:11 +0100

    Update MySQL_View.sql

--- MySQL_Database/MySQL_View.sql
+++ MySQL_Database/MySQL_View.sql
@@ -91,10 +91,11 @@ join zone zone_idx on boost.zone_id = zone_idx.id;
 -- Override View
 Drop View if exists override_view;
 CREATE VIEW override_view AS
-select override.`status`, override.sync, override.zone_id, zone_idx.index_id, zone.name, override.time, override.temperature
+select override.`status`, override.sync, override.purge, override.zone_id, zone_idx.index_id, zone_type.category, zone.name, override.time, override.temperature
 from override
 join zone on override.zone_id = zone.id
-join zone zone_idx on override.zone_id = zone_idx.id;
+join zone zone_idx on override.zone_id = zone_idx.id
+join zone_type on zone_type.type = zone.type;
 
 -- Schedule List with zone details view table version 1.x
 Drop View if exists schedule_night_climat_zone_view;
