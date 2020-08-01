9054e47d1075e5b047f9696476f2594c3c2b4b9f
twa127 <46624596+twa127@users.noreply.github.com>
2020-08-01 12:43:25 +0100

    Update MySQL_View.sql

--- MySQL_Database/MySQL_View.sql
+++ MySQL_Database/MySQL_View.sql
@@ -37,23 +37,24 @@ join zone zt on sdtz.zone_id = zt.id
 join zone_type ztype on zone.type = ztype.type
 where sdtz.`purge` = '0' order by zone.index_id;
 
--- Zone View version 2
+-- Zones View version 2
 Drop View if exists zone_view;
 CREATE VIEW zone_view AS
-select zone.status, zone.zone_status, zone.sync, zone.id, zone.index_id, zone.name, zone.type, ztype.category, zone.graph_it, zone.max_c, zone.max_operation_time, zone.hysteresis_time,
-zone.sp_deadband, sid.node_id as sensors_id, zone.sensor_child_id,
+select zone.status, zone.zone_state, zone.sync, zone.id, zone.index_id, zone.name, ztype.type, ztype.category, zone.graph_it, zs.max_c, zs.max_operation_time, zs.hysteresis_time,
+zs.sp_deadband, sid.node_id as sensors_id, zs.sensor_child_id,
 ctype.`type` AS controller_type, cid.node_id as controler_id, zone.controler_child_id,
 IFNULL(lasts.last_seen, lasts_2.last_seen) as last_seen, IFNULL(msv.ms_version, msv_2.ms_version) as ms_version, IFNULL(skv.sketch_version, skv_2.sketch_version) as sketch_version
 from zone
-join zone_type ztype on zone.type = ztype.type
-LEFT join nodes sid on zone.sensor_id = sid.id
+LEFT join zone_sensors zs on zone.id = zs.zone_id 
+join zone_type ztype on zone.type_id = ztype.id
+LEFT join nodes sid on zs.sensor_id = sid.id
 join nodes ctype on zone.controler_id = ctype.id
 join nodes cid on zone.controler_id = cid.id
-LEFT join nodes lasts on zone.sensor_id = lasts.id
+LEFT join nodes lasts on zs.sensor_id = lasts.id
 LEFT join nodes lasts_2 on zone.controler_id = lasts_2.id
-LEFT join nodes msv on zone.sensor_id = msv.id
+LEFT join nodes msv on zs.sensor_id = msv.id
 LEFT join nodes msv_2 on zone.controler_id = msv_2.id
-LEFT join nodes skv on zone.sensor_id = skv.id
+LEFT join nodes skv on zs.sensor_id = skv.id
 LEFT join nodes skv_2 on zone.controler_id = skv_2.id
 where zone.`purge` = '0';
 
@@ -86,7 +87,7 @@ select boost.id, boost.`status`, boost.sync, boost.zone_id, zone_idx.index_id, z
 from boost
 join zone on boost.zone_id = zone.id
 join zone zone_idx on boost.zone_id = zone_idx.id
-join zone_type on zone_type.type = zone.type;
+join zone_type on zone_type.id = zone.type_id;
 
 -- Override View
 Drop View if exists override_view;
@@ -95,21 +96,21 @@ select override.`status`, override.sync, override.purge, override.zone_id, zone_
 from override
 join zone on override.zone_id = zone.id
 join zone zone_idx on override.zone_id = zone_idx.id
-join zone_type on zone_type.type = zone.type;
+join zone_type on zone_type.id = zone.type_id;
 
 -- Schedule List with zone details view table version 1.x
 Drop View if exists schedule_night_climat_zone_view;
 CREATE VIEW schedule_night_climat_zone_view AS
 select tnct.id as time_id, tnct.status as time_status, snct.start_time as start, enct.end_time as end, snct.WeekDays, 
 nctz.sync as tz_sync, nctz.id as tz_id, nctz.status as tz_status, nctz.zone_id, zone.index_id, zone.name as zone_name, 
-zt.`type`, ztype.category, zone.status as zone_status, nctz.min_temperature, nctz.max_temperature
+ztype.`type`, ztype.category, zone.status as zone_status, nctz.min_temperature, nctz.max_temperature
 from schedule_night_climat_zone nctz
 join schedule_night_climate_time snct on nctz.schedule_night_climate_id = snct.id
 join schedule_night_climate_time enct on nctz.schedule_night_climate_id = enct.id
 join schedule_night_climate_time tnct on nctz.schedule_night_climate_id = tnct.id
 join zone on nctz.zone_id = zone.id
 join zone zt on nctz.zone_id = zt.id
-join zone_type ztype on zone.type = ztype.type
+join zone_type ztype on zone.type_id = ztype.id
 where nctz.`purge` = '0' order by zone.index_id;
 
 -- Messages_in View for Graps
