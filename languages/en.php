7d67b73800c954b9c0a89def9a84630864ce1e7a
twa127 <46624596+twa127@users.noreply.github.com>
2020-08-22 15:12:06 +0100

    Update en.php

--- languages/en.php
+++ languages/en.php
@@ -308,6 +308,7 @@ $lang['temperature_sensor_text'] = "Temperature sensor Node id, Battery level an
 $lang['max'] = "Max";
 $lang['sensor'] = "Sensor";
 $lang['ctr'] = "Ctr";
+$lang['controller'] = "Controller";
 $lang['smart_home_gateway'] = "Smart Home Gateway";
 $lang['smart_home_gateway_text_wifi'] = " Smart Home Gateway has nRF24L01 to communicate with the nodes and WiFi to connect to your home network to which controller will also be connected.";
 $lang['smart_home_gateway_text_serial'] = " Smart Home Gateway has nRF24L01 to communicate with the nodes and Serial to connect to your home automation controller.";
