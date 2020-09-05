ec68c929b652a8452097521bb0e5c12ec65f123a
twa127 <46624596+twa127@users.noreply.github.com>
2020-09-05 09:45:19 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -54,6 +54,70 @@
 
 <script>	
 $(document).ready(function() {
+    var maxField = 10; //Input fields increment limitation
+    var addButton = $('.add_button'); //Add button selector
+    var wrapper = $('.controler_id_wrapper'); //Input field wrapper
+//    var fieldHTML = '<div><input type="text" name="field_name[]" value=""/><a href="javascript:void(0);" class="remove_button"><img src="./images/remove-icon.png"/></a></div>'; //New input field html 
+
+    var controller_HTML = `
+                <div class="wrap">
+                        <!-- Zone Controller ID -->
+                        <div class="form-group" class="control-label"><label><?php echo $lang['zone_controller_id']; ?></label> <small class="text-muted"><?php echo $lang['zone_controler_id_info'];?></small>
+                                <input type="hidden" id="selected_controler_id[]" name="selected_controler_id[]" value="<?php echo $zone_controllers[$i]['controler_id']?>"/>
+                                <input type="hidden" id="selected_controler_child_id[]" name="selected_controler_child_id[]" value="<?php echo $zone_controllers[$i]['controler_child_id']?>"/>
+                                <input type="hidden" id="selected_controler_type[]" name="selected_controler_type[]" value="<?php echo $zone_controllers[$i]['zone_controller_type']?>"/>
+                                <div class="entry input-group col-xs-12">
+	                                <select id="controler_idx" onchange="ControlerChildList(this.options[this.selectedIndex].value, index_id)" name="controler_idx" class="form-control select2" data-error="<?php echo $lang['zone_controller_id_error']; ?>" autocomplete="off" >
+                                                <?php  $query = "SELECT node_id, type, max_child_id FROM nodes where name LIKE '%Controller%'  ORDER BY node_id ASC;";
+                                                $result = $conn->query($query);
+                                                echo "<option></option>";
+                                                while ($datarw=mysqli_fetch_array($result)) {
+                                                        echo "<option value=".$datarw['max_child_id'].">".$datarw['type'].' - '.$datarw['node_id']."</option>";
+                                                } ?>
+                                        </select>
+                                        <div class="help-block with-errors"></div>
+                                        <span class="input-group-btn">
+                                                <a href="javascript:void(0);" class="remove_button"><img src="./images/remove-icon.png"/></a>
+                                        </span>
+                                </div>
+                        </div>
+
+                        <input type="hidden" id="gpio_pin_list" name="gpio_pin_list" value="<?php echo implode(",", array_filter(Get_GPIO_List()))?>"/>
+                        <!-- Zone Controller Child ID -->
+                        <div class="form-group" class="control-label"><label><?php echo $lang['zone_controller_child_id']; ?></label> <small class="text-muted"><?php echo $lang['zone_controler_child_id_info'];?></small>
+		                <select id="controler_child_idx" name="controler_child_idx" onchange="setChild_id(this.options[this.selectedIndex].value, index_id)" class="form-control select2"  data-error="<?php echo $lang['zone_controller_child_id_error']; ?>" autocomplete="off">
+                                </select>
+                                <div class="help-block with-errors"></div>
+                        </div>
+                </div>
+		`;
+
+    //Once add button is clicked
+    $(addButton).click(function(){
+        //Check maximum number of input fields
+	var x = document.getElementById("controller_count").value
+        var temp_HTML = controller_HTML.replace(/controler_idx/g, "controler_id".concat(x));
+	temp_HTML = temp_HTML.replace(/controler_child_idx/g, "controler_child_id".concat(x));
+        temp_HTML = temp_HTML.replace(/index_id/g, x);
+        if(x < maxField){ 
+            $(wrapper).append(temp_HTML); //Add field html
+            x++; //Increment field counter
+	    document.getElementById("controller_count").value = x;
+        }
+    });
+    
+    //Once remove button is clicked
+    $(wrapper).on('click', '.remove_button', function(e){
+	var x = document.getElementById("controller_count").value
+        e.preventDefault();
+        x--; //Decrement field counter
+        document.getElementById("controller_count").value = x;
+        $(this).parents('.wrap:first').remove(); //Remove field html
+    });
+
+});
+
+$(document).ready(function() {
 //delete record 
 $('#confirm-delete').on('show.bs.modal', function(e) {
     $(this).find('.btn-ok').attr('href', $(e.relatedTarget).data('href'));
